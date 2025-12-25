31b7e14b48637f62f1f9a12e4c9d5ce581630048
Conmeosoma <dinhhoat157@gmail.com>
2025-12-25 22:28:56 +0700

    Update code Thu 12/25/2025 22:28:56.57

--- My2DGame/src/Main/GamePanel.java
+++ My2DGame/src/Main/GamePanel.java
@@ -4,6 +4,7 @@
 // > ^ <
 package Main;
 
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
@@ -17,9 +18,13 @@ public class GamePanel extends JPanel { // khai bao lop GamePanel ke thua JPanel
   final int maxScreenRow = 12; // gioi han so hang cua man hinh
   final int screenWidth = tileSize * maxScreenCol; // 768 pixel
   final int screenHeight = tileSize * maxScreenRow; // 576 pixel
+ 
 
   public GamePanel() {
-    this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+    this.setPreferredSize(new Dimension(screenWidth, screenHeight)); // dat kich thuoc panel
+    this.setBackground(Color.black); // dat mau nen den
+    this.setDoubleBuffered(true); // cai dat double buffering de giam nhieu lan ve
+    
   }
 
 }
--- My2DGame/src/Main/Main.java
+++ My2DGame/src/Main/Main.java
@@ -2,20 +2,24 @@
 // /\_/\  
 //( o.o ) 
 // > ^ <
-
 package Main;
 
 import javax.swing.JFrame;
 
 public class Main {
+
     public static void main(String[] args) {
         JFrame window = new JFrame();// Khởi tạo cửa sổ JFrame
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Đóng cửa số khi nhấn nút X
+
         window.setResizable(false); // Khong cho phep thay doi kich thuoc cua cua so
         window.setTitle("2D Adventure Game"); // Tieu de cua cua so
+
+        GamePanel gamePanel = new GamePanel(); // tao mot doi tuong GamePanel
+        window.add(gamePanel); // them GamePanel vao cua so
+        window.pack(); // dieu chinh kich thuoc cua so de phu hop voi GamePanel
+
         window.setLocationRelativeTo(null); // cua so o giua man hinh
         window.setVisible(true); // hien thi cua so
-  
     }
-
 }
